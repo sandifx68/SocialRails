850e326def48c25998661d7b4d5ac844a4c81222
sandifx68 <sandixd333@gmail.com>
2025-02-20 21:35:13 +0100

    Created user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root "posts#index"
-  get "/posts", to: "posts#index"
+  resources :users, only: [ :new, :create, :show, :edit, :update ]
 
-  resources :users, only: [ :new, :create ]
+  root "posts#index"
+  get "posts", to: "posts#index"
+
 
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy", as: :logout
+  delete "logout", to: "sessions#destroy", as: :logout
 end
