6a5b81617809de40ac2bc7a85cfbf8dc9710f701
sandifx68 <sandixd333@gmail.com>
2025-05-04 12:04:26 +0200

    profil photo tests

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe "User show", type: :feature do
     end
   end
 
-  scenario "allows logged user to modify own name" do
+  scenario "allows logged user to modify own editables" do
     log_in_as(user)
     visit user_path(user)
 
@@ -42,6 +42,71 @@ RSpec.describe "User show", type: :feature do
     editables.each do |e|
       expect(page).to have_selector("#edit-#{e}-link")
     end
+  end
+
+  scenario "user can modify name" do
+    log_in_as(user)
+    visit user_path(user)
+
+    click_link(user.display_name)
+    fill_in "user_display_name", with: "new_user_name"
+    click_button "Save"
+    expect(page).to have_content "new_user_name"
+  end
+
+  scenario "user can modify pfp" do
+    log_in_as(user)
+    visit user_path(user)
+
+    img = page.find("img.profile-photo.profile-photo-editable")
+    expected_src = ActionController::Base.helpers.asset_path("default_pfp.jpg")
+    expect(img[:src]).to include(expected_src)
+    click_link "edit-profile-photo-link"
+    attach_file "user_profile_photo", Rails.root.join("app/assets/images/alt_pfp.jpg")
+    click_button "Save"
+    img_new = page.find("img.profile-photo.profile-photo-editable")
+    expect(img_new[:src]).to include("alt_pfp")
+  end
+
+  scenario "user can modify bg" do
+    log_in_as(user)
+    visit user_path(user)
+
+    img = page.find("div.background-photo")[:style]
+    expected_asset = ActionController::Base.helpers.asset_path("default_bg.jpg")
+    expect(img).to include(expected_asset)
+    click_link "edit-background-image-link"
+    attach_file "user_background_image", Rails.root.join("app/assets/images/alt_pfp.jpg")
+    click_button "Save"
+    img_new = page.find("div.background-photo")[:style]
+    expect(img_new).to include("alt_pfp")
+  end
+
+  scenario "user cannot add too big of a pfp" do
+    log_in_as(user)
+    visit user_path(user)
+
+    img = page.find("img.profile-photo.profile-photo-editable")
+    expected_src = ActionController::Base.helpers.asset_path("default_pfp.jpg")
+    expect(img[:src]).to include(expected_src)
+    click_link "edit-profile-photo-link"
+    attach_file "user_profile_photo", Rails.root.join("app/assets/images/pfp_too_big.jpg")
+    click_button "Save"
+    expect(page).to have_content "Uploaded image is too large (max 5MB)"
+    # current_img = page.find("img.profile-photo.profile-photo-editable")
+    # expect(current_img[:src]).to include(expected_src)
+  end
+
+  scenario "user cannot add empty pfp" do
+    log_in_as(user)
+    visit user_path(user)
+
+    img = page.find("img.profile-photo.profile-photo-editable")
+    expected_src = ActionController::Base.helpers.asset_path("default_pfp.jpg")
+    expect(img[:src]).to include(expected_src)
+    click_link "edit-profile-photo-link"
+    click_button "Save"
+    expect(page).to have_content "Profile photo must be selected before uploading"
   end
 
   scenario "doesn't show posts beloning to other user" do
